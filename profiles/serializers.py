62028a362093f363e4976ee5f4fcdbdd6ca1bcd5
Alan <alanlove@email.arizona.edu>
2014-01-06 10:14:20 -0700

    removed some userprofile fields from visible api

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -6,56 +6,67 @@ from profiles.models import *
 class SteamProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = SteamProfile
+        fields = ('username', 'url', 'userid',)
 
 
 class SomethingAwfulProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = SomethingAwfulProfile
+        fields = ('username', 'url', 'userid', 'postcount', 'regdate',)
 
 
 class LeagueOfLegendsProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = LeagueOfLegendsProfile
+        fields = ('username',)
 
 
 class EveOnlineProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = EveOnlineProfile
+        fields = ('username',)
 
 
 class MinecraftProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = MinecraftProfile
+        fields = ('username',)
 
 
 class NintendoProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = NintendoProfile
+        fields = ('friendcode',)
 
 
 class PlaystationNetworkProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = PlaystationNetworkProfile
+        fields = ('username',)
 
 
 class XboxLiveProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = XboxLiveProfile
+        fields = ('gamertag',)
 
 
 class BattlefieldFourProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = BattlefieldFourProfile
+        fields = ('username',)
 
 
 class BlizzardProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = BlizzardProfile
+        fields = ('realid', 'email',)
         
 
 class WorldOfTanksProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = WorldOfTanksProfile
+        fields = ('username',)
         
 
 class UserSerializer(serializers.ModelSerializer):
